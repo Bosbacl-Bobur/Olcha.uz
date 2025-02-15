c4f303f25ee334a8b9137f5ca9e3ccb6116a69c3
Sening isming <Emailing>
2025-02-15 16:53:30 +0500

    add

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./public**/*figma.html"],
+  theme: {
+    extend: {
+      backgroundColor:{
+        'bodyColor':'#F8FFFF'
+      },
+      container:{
+        '80%':'80%'
+      },
+      mt:{
+        'mrg':'900px'
+      }
+    },
+  },
+  plugins: [],
+}
+
